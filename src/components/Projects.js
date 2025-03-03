84a5d22ba441426614c8841b3ea681c6eabee0af
dega086 <degadhanush77@gmail.com>
2025-03-03 20:31:17 +0530

    changes-2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,14 @@ function Projects() {
       <h2>My Projects</h2>
       <div className="project-card">
         <h3>Project 1</h3>
-        <p>Description of the project.</p>
+        <p>Mini Project.</p>
         <a href="https://github.com/dega086/React-mini-project.git" target="_blank" rel="noopener noreferrer">
           View Project
         </a>
       </div>
       <div className="project-card">
         <h3>Project 2</h3>
-        <p>Description of the project.</p>
+        <p>React project.np</p>
         <a href="https://github.com/dega086/React-mini-project.git" target="_blank" rel="noopener noreferrer">
           View Project
         </a>
